95232298da788479ca489b84e2638e9d7c61903a
christine-barton <100629845+christine-barton@users.noreply.github.com>
2022-04-04 17:30:18 -0500

    editing .html files

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', pageTitle= 'Welcome to VTM')
 
 @app.route('/about')
-def mike():
-    return render_template('about.html')
+def about():
+    return render_template('about.html', pageTitle= 'About Vertical Tank Maintenance')
 
 @app.route('/estimate')
-def mike():
-    return render_template('estimate.html')
+def estimate():
+    return render_template('estimate.html', pageTitle= 'VTM Estimator')
 
 if __name__ == '__main__':
     app.run(debug=True)
