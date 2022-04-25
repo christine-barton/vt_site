a37da71cfe026c0f4be044cc0d841b6c11cb7ff9
christine-barton <100629845+christine-barton@users.noreply.github.com>
2022-04-25 18:15:22 -0500

    Create test_estimate.py

---
+++ tests/units/test_estimate.py
@@ -0,0 +1,10 @@
+import app
+
+def test_calculate_estimate():
+     """
+     GIVEN a user enters a radius and height
+     WHEN that radius and height is passed to this function
+     THEN the total_estimate is accurately calculated
+     """
+     assert app.calculate_estimate(180,360) == "141300.0"
+     
